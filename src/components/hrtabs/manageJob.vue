a619b61db2cecac89141af4c0cf59decd4c260ec
claire <1747527076@qq.com>
2018-06-24 19:42:20 +0800

    管理职位修改bug

--- src/components/hrtabs/manageJob.vue
+++ src/components/hrtabs/manageJob.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <div v-if="show" class="nofind">
-      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524746733938&di=9ae24b5205e97c2876b48d3ff26f1c23&imgtype=0&src=http%3A%2F%2Fwww.snlfjx.com%2Fforum%2FSkin%2Fimgs%2Fno-data.png">
+      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524746733938&di=9ae24b5205e97c2876b48d3ff26f1c23&imgtype=0&src=http%3A%2F%2Fwww.snlfjx.com%2Fforum%2FSkin%2Fimgs%2Fno-data.png" style="margin: 0 auto">
       <p>暂时没有发布的职位哦，快去发布吧</p>
     </div>
     <el-card  v-for="(item, key) in joblist" :key="key" class="mycard" v-if="!show" @click="checkDetail(item.id)">
